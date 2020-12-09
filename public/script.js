e5e1166f7a056cacb9f6240a83dc0f88e0554872
Fatih Samur <a.fatihsamur@gmail.com>
2020-12-10 00:02:50 +0300

    version 1.0

--- public/script.js
+++ public/script.js
@@ -8,13 +8,15 @@ const peer = new Peer(undefined, {
 });
 
 const myVideo = document.createElement('video');
-myVideo.muted = true;
+
+let myVideoStream;
 navigator.mediaDevices
   .getUserMedia({
     video: true,
-    audio: false,
+    audio: true,
   })
   .then((stream) => {
+    myVideoStream = stream;
     addVideoStream(myVideo, stream);
 
     peer.on('call', (call) => {
@@ -52,4 +54,91 @@ const addVideoStream = (video, stream) => {
     video.play();
   });
   videoGrid.append(video);
+};
+
+const msg = document.getElementById('chat_message');
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter' && msg.value !== '') {
+    socket.emit('message', msg.value);
+    msg.value = '';
+  }
+});
+
+socket.on('createMessage', (message) => {
+  const msgList = document.getElementById('msg_list');
+  const listItem = document.createElement('li');
+  listItem.classList.add('msg_list');
+  listItem.innerHTML = ` <p class="message" > User: ${message} </> <p/>  `;
+
+  msgList.appendChild(listItem);
+});
+
+const muteBtn = document.getElementById('mute_btn');
+
+// mute audio
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+};
+
+muteBtn.addEventListener('click', () => {
+  muteUnmute();
+});
+
+const setUnmuteButton = () => {
+  const html = `
+  <i class="red-text fas fa-microphone-slash"></i>
+  <span class="red-text" > Unmute </span>
+  `;
+  muteBtn.innerHTML = html;
+};
+
+const setMuteButton = () => {
+  const html = `
+  <i class="fas fa-microphone"></i>
+  <span> Mute </span>
+  `;
+  muteBtn.innerHTML = html;
+};
+
+// stop video
+
+const stopStartVideo = () => {
+  const enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setStopVideoButton();
+  } else {
+    setStartVideoButton();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+};
+
+const stopVideoBtn = document.getElementById('stop_video_btn');
+
+stopVideoBtn.addEventListener('click', () => {
+  stopStartVideo();
+});
+
+const setStopVideoButton = () => {
+  const html = `
+  <i class="red-text fas fa-video-slash"></i>
+  <span class="red-text" > Start Video </span>
+  `;
+  stopVideoBtn.innerHTML = html;
+};
+
+const setStartVideoButton = () => {
+  const html = `
+  <i class="fas fa-video"></i>
+  <span> Stop Video </span>
+  `;
+  stopVideoBtn.innerHTML = html;
 };
