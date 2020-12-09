519013d1b8c3b4f07b5b7b225f38d3ed242b4bff
Fatih Samur <a.fatihsamur@gmail.com>
2020-12-10 00:09:58 +0300

    v1.0

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
